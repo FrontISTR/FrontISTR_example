6449df0a39135449a7b7d6aeab3fe376b567cea1
Yu IHARA <ihara@multi.k.u-tokyo.ac.jp>
2017-07-05 01:06:47 +0900

    Initial commit

--- run.sh
+++ run.sh
@@ -9,12 +9,14 @@ else
   echo "Not N"
   exit
 fi
+find . -name '*.gz' -exec gzip -d {} \;
+
 PWD=`pwd`
 DATE=`date '+%Y%m%d%H%M%S'`
 cd $2
 echo '!PARTITION,TYPE=NODE-BASED,METHOD=KMETIS,DOMAIN='$PROCS > hecmw_part_ctrl.dat
 export OMP_NUM_THREADS=4
-mpirun -np 1 -x OMP_NUM_THREADS=$OMP_NUM_THREADS hecmw_part1 2>&1 | tee $PWD/$DATEpart1.log
+mpirun -np 1 -x OMP_NUM_THREADS=$OMP_NUM_THREADS hecmw_part1 2>&1 | tee "$PWD/$DATE"_part1.log
 export OMP_NUM_THREADS=1
-mpirun -np $PROCS -x OMP_NUM_THREADS=$OMP_NUM_THREADS fistr1 2>&1 | tee $PWD/$DATEfistr1.log
+mpirun -np $PROCS -x OMP_NUM_THREADS=$OMP_NUM_THREADS fistr1 2>&1 | tee "$PWD/$DATE"_fistr1.log
 cd -
